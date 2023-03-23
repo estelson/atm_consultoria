b42f86d20bb9c8539545144217d02fae7441b1fc
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 20:13:12 -0300

    Configuração da estrutura da HomePage e imagens utilizadas no app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,11 +11,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("ATM Consultoria"),
         backgroundColor: Colors.green,
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/images/logo.png"),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Image.asset("assets/images/menu_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Image.asset("assets/images/menu_servico.png"),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Image.asset("assets/images/menu_cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Image.asset("assets/images/menu_contato.png"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

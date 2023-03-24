a1de1da58547e94325784d6a370b9b86338a7cab
Estelson Medeiros Pereira <estelson@gmail.com>
2023-03-23 21:02:27 -0300

    Correção do posicionamento dos objetos na HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,46 +16,55 @@ class _HomePageState extends State<HomePage> {
         title: const Text("ATM Consultoria"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/images/logo.png"),
-            Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Image.asset("assets/images/menu_empresa.png"),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/logo.png"),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset("assets/images/menu_empresa.png"),
+                        ),
+                        const SizedBox(width: 32),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset("assets/images/menu_servico.png"),
+                        )
+                      ],
+                    ),
                   ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Image.asset("assets/images/menu_servico.png"),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset("assets/images/menu_cliente.png"),
+                        ),
+                        const SizedBox(width: 32),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset("assets/images/menu_contato.png"),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Image.asset("assets/images/menu_cliente.png"),
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Image.asset("assets/images/menu_contato.png"),
-                  )
-                ],
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
